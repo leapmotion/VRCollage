df3616841cba79f201c628a2f0e9226807481dc1
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2015-02-27 14:57:16 -0800

    Oops, fix chrome fullscreen hotkey

--- javascript/interface.js
+++ javascript/interface.js
@@ -1,7 +1,7 @@
 (function() {
 
   var onkey = function(event) {
-    if (event.key === 'z' || event.keyCode == 102) {
+    if (event.key === 'z' || event.keyCode == 122) {
       vrControls.zeroSensor();
     }
     if (event.key === 'f' || event.keyCode == 102) {
